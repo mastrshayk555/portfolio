301847474110864fc99ce704ca0c46479cc46676
jeff <jeffreyheim55@gmail.com>
2022-11-15 01:13:15 -0500

    reorg and use django templating

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index.as_view(), name='index')
+    path('', views.home.as_view(), name='home'),
+    path('about', views.about.as_view(), name='about'),
+    path('works', views.works.as_view(), name='works'),
+    path('contact', views.contact.as_view(), name='contact'),
+    path('components', views.components.as_view(), name='components'),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -2,6 +2,22 @@ from django.shortcuts import render
 from django.views.generic import DetailView
 
 
-class index(DetailView):
+class home(DetailView):
     def get(self, request):
-        return render(request, 'mysite/index.html')
+        return render(request, 'mysite/home.html')
+
+class about(DetailView):
+    def get(self, request):
+        return render(request, 'mysite/about.html')
+
+class works(DetailView):
+    def get(self, request):
+        return render(request, 'mysite/projects.html')
+        
+class contact(DetailView):
+    def get(self, request):
+        return render(request, 'mysite/contact.html')
+
+class components(DetailView):
+    def get(self, request):
+        return render(request, 'mysite/components.html')
